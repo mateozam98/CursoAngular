a22ef3fc1229906907fb9e7a59eb18acc4cbdbd1
mateozam98 <mateozzz123@gmail.com>
2020-07-29 13:26:46 -0500

    Agregamos componentes en lista de destinos..

--- angular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ angular-whishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from './../../app/models/destino-viaje.model';
 
 @Component({
   selector: 'app-lista-destinos',
@@ -6,12 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-  destinos: string[];
+  destinos: DestinoViaje[];
   constructor() { 
-  	this.destinos = ['Barranquilla','Lima','Buenos Aires','Barcelona','Quito']
+  	this.destinos = []
   }
 
   ngOnInit(): void {
   }
 
+  guardar(nombre:string, url:string):boolean {
+  	this.destinos.push(new DestinoViaje(nombre, url));
+  	return false;
+  }
 }
