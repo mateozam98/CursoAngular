64d49517fc7a8adf28fb9335db78dc759a2f2d2d
mateozam98 <mateozzz123@gmail.com>
2020-08-05 16:16:56 -0500

    Ingreso de Cypress

--- angular-whishlist/src/app/models/destinos-api-client.model.ts
+++ angular-whishlist/src/app/models/destinos-api-client.model.ts
@@ -1,18 +1,72 @@
-import { DestinoViaje} from './destino-viaje.model';
+import { DestinoViaje } from './destino-viaje.model';
 import { Subject, BehaviorSubject } from 'rxjs';
+
+//redux
+import { Injectable, Inject, forwardRef } from '@angular/core';
+import { tap, last } from 'rxjs/operators';
+import { Action } from '@ngrx/store';
 import { Store } from '@ngrx/store';
-import { AppState } from '../app.module';
-import { NuevoDestinoAction, ElegidoFavoritoAction } from './destinos-viajes-state.model';
-import { Injectable } from '@angular/core';
+import {
+    DestinosViajesState,
+    NuevoDestinoAction,
+    ElegidoFavoritoAction
+} from './destinos-viajes-state.model';
+import { AppState, APP_CONFIG, AppConfig, db, MyDatabase } from './../app.module';
+import { HttpRequest, HttpHeaders, HttpClient, HttpEvent, HttpResponse } from '@angular/common/http';
 
 @Injectable()
-export class DestinoApiClient {
-    constructor(private store: Store<AppState>){
+export class DestinosApiClient {
+
+    destinos: DestinoViaje[] = [];
+    constructor(
+        private store: Store<AppState>,
+        @Inject(forwardRef(() => APP_CONFIG)) private config: AppConfig,
+        private http: HttpClient
+    ) {
+        this.store
+            .select(state => state.destinos)
+            .subscribe((data) => {
+                console.log('destinos sub store');
+                console.log(data);
+                this.destinos = data.items;
+            });
+        this.store
+            .subscribe((data) => {
+                console.log('all store');
+                console.log(data);
+            });
     }
-    add(d:DestinoViaje){
-        this.store.dispatch(new NuevoDestinoAction(d));
+
+
+    add(d: DestinoViaje) {
+        const headers: HttpHeaders = new HttpHeaders({ 'X-API-TOKEN': 'token-seguridad' });
+        const req = new HttpRequest('POST', this.config.apiEndpoint + '/my', { nuevo: d.nombre }, { headers: headers });
+        this.http.request(req).subscribe((data: HttpResponse<{}>) => {
+            if (data.status === 200) {
+                this.store.dispatch(new NuevoDestinoAction(d));
+                const myDb = db;
+                myDb.destinos.add(d);
+                console.log('todos los destinos de la db!');
+                myDb.destinos.toArray().then(destinos => console.log(destinos))
+            }
+        });
     }
-    elegir(d: DestinoViaje){
+
+
+    getById(id: String): DestinoViaje {
+        return this.destinos.filter((d) => { return d.id.toString() == id; })[0]
+
+    }
+
+    getAll(): DestinoViaje[] {
+        return this.destinos;
+
+    }
+
+
+    elegir(d: DestinoViaje) {
         this.store.dispatch(new ElegidoFavoritoAction(d));
     }
+
+
 }
--- angular-whishlist/src/app/models/destinos-viajes-state.model.ts
+++ angular-whishlist/src/app/models/destinos-viajes-state.model.ts
@@ -1,100 +1,113 @@
-import {Injectable} from '@angular/core';
-import {Action, State} from '@ngrx/store';
-import {Actions, Effect, ofType} from '@ngrx/effects';
-import {Observable, of} from 'rxjs';
-import {map} from 'rxjs/operators';
-import {DestinoViaje} from './destino-viaje.model';
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { DestinoViaje } from './destino-viaje.model';
 
 //ESTADO
-export interface DestinosViajesState {
+export interface DestinosViajesState{
     items: DestinoViaje[];
     loading: boolean;
-    favorite: DestinoViaje;
+    favorito: DestinoViaje;
 }
 
-export const inicializeDestinosViajesState = function(){
-    return{
-        items: [],
-        loading: false,
-        favorite: null
-    };
-};
+export function intializeDestinosViajesState() {
+  return {
+    items: [],
+    loading: false,
+    favorito: null
+  };
+}
 
 //ACCIONES
 export enum DestinosViajesActionTypes {
-    NUEVO_DESTINO ='[Destinos Viajes] Nuevo',
-    ELEGIDO_FAVORITO ='[Destinos Viajes] Favoritos',
-    VOTE_UP ='[Destinos Viajes] Vote Up',
-    VOTE_DOWN ='[Destinos Viajes] vote Down'
+  NUEVO_DESTINO = '[Destinos Viajes] Nuevo',
+  ELEGIDO_FAVORITO = '[Destinos Viajes] Favorito',
+  VOTE_UP = '[Destinos Viajes] Vote Up',
+  VOTE_DOWN = '[Destinos Viajes] Vote Down',
+  INIT_MY_DATA = '[Destinos Viajes] Init My Data'
 }
 
 export class NuevoDestinoAction implements Action {
-    type = DestinosViajesActionTypes.NUEVO_DESTINO;
-    constructor(public destino: DestinoViaje) {}
+  type = DestinosViajesActionTypes.NUEVO_DESTINO;
+  constructor(public destino: DestinoViaje) {}
 }
 
 export class ElegidoFavoritoAction implements Action {
-    type = DestinosViajesActionTypes.ELEGIDO_FAVORITO;
-    constructor (public destino: DestinoViaje){}
+  type = DestinosViajesActionTypes.ELEGIDO_FAVORITO;
+  constructor(public destino: DestinoViaje) {}
 }
 
 export class VoteUpAction implements Action {
     type = DestinosViajesActionTypes.VOTE_UP;
     constructor(public destino: DestinoViaje) {}
-}
-
-export class VoteDownAction implements Action {
+  }
+  
+  export class VoteDownAction implements Action {
     type = DestinosViajesActionTypes.VOTE_DOWN;
-    constructor (public destino: DestinoViaje){}
-}
+    constructor(public destino: DestinoViaje) {}
+  }
 
-export type DestinosViajesActions = NuevoDestinoAction | ElegidoFavoritoAction | VoteUpAction | VoteDownAction;
+  export class InitMyDataAction implements Action {
+    type = DestinosViajesActionTypes.INIT_MY_DATA;
+    constructor(public destinos: string[]) {}
+  }
 
-//REDUCES
+export type DestinosViajesActions = NuevoDestinoAction | ElegidoFavoritoAction
+  | VoteUpAction | VoteDownAction | InitMyDataAction;
+
+//REDUCERS
 export function reducerDestinosViajes(
-    state: DestinosViajesState,
-    action: DestinosViajesActions
-): DestinosViajesState {
-    switch (action.type){
-        case DestinosViajesActionTypes.NUEVO_DESTINO: {
-            return {
-                ...state,
-                items: [...state.items, (action as NuevoDestinoAction).destino]
-            };
-        }
-        case DestinosViajesActionTypes.ELEGIDO_FAVORITO: {
-            state.items.forEach(x => x.setSelected(false));
-            const fav: DestinoViaje = (action as ElegidoFavoritoAction).destino;
-            fav.setSelected(true);
-            return{
-                ...state,
-                favorite: fav
-            };
+	state:DestinosViajesState,
+	action:DestinosViajesActions
+) : DestinosViajesState {
+	switch (action.type) {
+    case DestinosViajesActionTypes.INIT_MY_DATA: {
+      const destinos: string[] = (action as InitMyDataAction).destinos;
+      return {
+          ...state,
+          items: destinos.map((d) => new DestinoViaje(d, ''))
+        };
+    }
+    
+		case DestinosViajesActionTypes.NUEVO_DESTINO: {
+		  return {
+		  		...state,
+		  		items: [...state.items, (action as NuevoDestinoAction).destino ]
+		  	};
+		}
+		case DestinosViajesActionTypes.ELEGIDO_FAVORITO: {
+		    state.items.forEach(x => x.setSelected(false));
+		    let fav:DestinoViaje = (action as ElegidoFavoritoAction).destino;
+		    fav.setSelected(true);
+		    return {
+		    	...state,
+		  		favorito: fav
+		    };
         }
         case DestinosViajesActionTypes.VOTE_UP: {
-            state.items.forEach(x => x.setSelected(false));
             const d: DestinoViaje = (action as VoteUpAction).destino;
             d.voteUp();
-            return{...state };
+            return { ...state };
         }
         case DestinosViajesActionTypes.VOTE_DOWN: {
-            state.items.forEach(x => x.setSelected(false));
             const d: DestinoViaje = (action as VoteDownAction).destino;
             d.voteDown();
-            return{...state };
+            return { ...state };
         }
-    }
-    return state;
+	}
+	return state;
 }
 
 //EFFECTS
 @Injectable()
 export class DestinosViajesEffects {
-    @Effect()
-    nuevoAgregado$: Observable<Action> = this.actions$.pipe(
-        ofType(DestinosViajesActionTypes.NUEVO_DESTINO),
-        map((action: NuevoDestinoAction) => new ElegidoFavoritoAction(action.destino))
-    );
+  @Effect()
+  nuevoAgregado$: Observable<Action> = this.actions$.pipe(
+    ofType(DestinosViajesActionTypes.NUEVO_DESTINO),
+  	map((action:NuevoDestinoAction) => new ElegidoFavoritoAction(action.destino))
+  );
 
-    constructor(private actions$: Actions){}
-}
+  constructor(private actions$: Actions) {}
+} 
